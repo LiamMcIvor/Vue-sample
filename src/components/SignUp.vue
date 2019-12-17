5de0eff6e87e3f8979ef0d799512ce35aabb9363
LiamMcIvor <liam.mcivor1@gmail.com>
2019-12-17 17:42:10 +0000

    form validation added

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,23 +1,51 @@
 <template>
     <div id="signUpForm">
-        <v-text-field v-model="placeholder" label="First Name"></v-text-field>
-        <v-text-field v-model="placeholder" label="Surname"></v-text-field>
-        <v-text-field v-model="placeholder" label="Street Name"></v-text-field>
-        <v-text-field v-model="placeholder" label="House Number"></v-text-field>
-        <v-text-field v-model="placeholder" label="Postcode"></v-text-field>
-        <v-text-field v-model="placeholder" label="City"></v-text-field>
-        <v-text-field v-model="placeholder" label="Country"></v-text-field>
-        <v-text-field v-model="placeholder" label="E-mail Address"></v-text-field>
-        <v-text-field v-model="placeholder" label="Confirm E-mail"></v-text-field>
-        <v-text-field v-model="password"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[rules.required, rules.min]"
-              :type="show1 ? 'text' : 'password'"
-              label="Normal with hint text"
-              hint="At least 8 characters"
-              counter
-              ></v-text-field>
-        <v-text-field v-model="placeholder" label="Confirm Password"></v-text-field>
+        <v-text-field
+         v-model="name" label="First Name"
+         :rules="[rules.required]"
+         :type="'String'"
+        ></v-text-field>
+        <v-text-field
+         v-model="placeholder" label="Surname"
+         :rules="[rules.required]"
+        ></v-text-field>
+        <v-text-field
+         v-model="placeholder" label="Street Name"
+         :rules="[rules.required]"
+        ></v-text-field>
+        <v-text-field
+         v-model="placeholder" label="House Number"
+         :rules="[rules.required]"
+         :type="'number'"
+        ></v-text-field>
+        <v-text-field
+         v-model="placeholder" label="Postcode"
+         :rules="[rules.required, rules.pattern]"
+        ></v-text-field>
+        <v-text-field
+         v-model="placeholder" label="City"
+         :rules="[rules.required]"
+        ></v-text-field>
+        <v-text-field 
+          v-model="placeholder" label="Country"
+          :rules="[rules.required]"
+        ></v-text-field>
+        <v-text-field 
+          v-model="placeholder" label="E-mail Address"
+          :rules="[rules.required]"
+        ></v-text-field>
+        <v-text-field 
+          v-model="placeholder" label="Confirm E-mail"
+          :rules="[rules.required]"  
+        ></v-text-field>
+        <v-text-field 
+          v-model="password" label="Password"
+          :rules="[rules.required, rules.min]"
+        ></v-text-field>
+        <v-text-field 
+          v-model="placeholder" label="Confirm Password"
+          :rules="[rules.required, rules.min]"
+        ></v-text-field>
         <b-button href="#" variant="outline-primary">Submit</b-button>
     </div>
     
@@ -26,19 +54,18 @@
 <script>
 export default {
     data () {
-    return {
-    //   show1: false,
-    //   show2: true,
-    //   show3: false,
-    //   show4: false,
-      password: 'Password',
-      rules: {
-        required: value => !!value || 'Required.',
-        min: v => v.length >= 8 || 'Min 8 characters',
-        emailMatch: () => ('The email and password you entered don\'t match'),
-      },
-    }
-  },
+      return {
+        // pattern: {
+        //   pattern: '/^[a-z]{2}\d{1,2}\s*\d[a-z]{2}$/i'
+        // },
+        rules:  {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters', 
+          message:'One lowercase letter required.',
+          // pattern:"/^[a-z]{2}\d{1,2}\s*\d[a-z]{2}$/i"
+        }
+      }
+    },
     name: 'SignUp'
 }
 
